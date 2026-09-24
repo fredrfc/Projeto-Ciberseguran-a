6039404962a1731feb95b9a07b1393b7ab89cd5c
fredrfc <frederico.cajado@ufu.br>
2026-09-24 11:08:02 -0300

    Permite buscar ativos pelo ID ou Hostname

--- buscar ativos.py
+++ buscar ativos.py
@@ -1,10 +1,10 @@
 import pprint
 def buscar_ativos(dict_ativos):
     try:
-        print('=' * 30)
+        print('=' * 26)
         print('SISTEMA DE BUSCA DE ATIVOS')
-        print('=' * 30)
-        print('\n1. ID')
+        print('=' * 26)
+        print('1. ID')
         print('2. Hostname')
         
         tipo_busca = input('\nDigite [1] para buscar ativos pelo ID ou [2] para buscar pelo Hostname: ').strip()
@@ -31,8 +31,6 @@ def buscar_ativos(dict_ativos):
                 print(f'Ativo: {id_busca} encontrado! Carregando informações...\n')
                 print('=' * 30)
                 pprint(dict_ativos[id_busca])
-                print('VULNERABILIDADES ASSOCIADAS A ESSE ATIVO:')
-                pprint(vulnerabilidade_nova[id_busca])
                 print('=' * 30)
                 return True
             
@@ -48,17 +46,22 @@ def buscar_ativos(dict_ativos):
                 print('O campo não pode estar vazio!')
                 return False
             
-            if hostname_busca in dict_ativos:
-                print(f'Ativo: {hostname_busca} encontrado! Carregando informações...\n')
-                print('=' * 30)
-                pprint(dict_ativos[hostname_busca])
-                print('VULNERABILIDADES ASSOCIADAS A ESSE ATIVO:')
-                pprint(vulnerabilidade_nova[id_busca])
-                print('=' * 30)
+            encontrado = False
 
-            else:
+            for id_ativo, dados in dict_ativos.items():
+                if dados['Nome/Hostname'].strip().lower() == hostname_busca:
+                    print(f'\nAtivo: {dados['Nome/Hostname']} encontrado! Carregando informações...')
+                    print('=' * 30)
+                    pprint(dados)
+                    print('=' * 30)
+                    encontrado = True
+                    break
+
+            if not encontrado:
                 print(f'Não foi possível encontrar um ativo com o nome/hostname: {hostname_busca}')
                 return False
+
+            return True
 
         else:
             print('Entrada inválida! Digite [1] para buscar um ativo pelo ID ou [2] para buscar pelo Hostname.')
@@ -72,4 +75,3 @@ def buscar_ativos(dict_ativos):
 
     except Exception as e:
         print(f'Erro! Ocorreu um erro do tipo: {e}')
-
